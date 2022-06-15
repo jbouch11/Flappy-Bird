8151991785d277d9c8fe78892c2dafc249ac6cbe
Jack Boucher <jboucher56@gmail.com>
2022-06-14 20:00:15 -0500

    Styling applied to character ID

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 
 hole.addEventListener('animationiteration', () => {
-
+    var random = Math.random()*3;
+    var top = (random*100)+150;
+    hole.style.top = -(top) + "px";
 });
 
