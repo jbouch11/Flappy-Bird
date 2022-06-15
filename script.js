43a15f14290f9707b8afc302531ed4042e0a4369
Jack Boucher <jboucher56@gmail.com>
2022-06-14 20:05:54 -0500

    added interval function to simulate gravity for the ball

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
+var character = document.getElementById("character");
 
 hole.addEventListener('animationiteration', () => {
     var random = Math.random()*3;
     var top = (random*100)+150;
     hole.style.top = -(top) + "px";
 });
+setInterval(function(){
+    var characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    character.style.top = (characterTop+3) + "px";
+},10);
 
