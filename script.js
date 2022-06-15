b83b7ea45375d7a57531a54da835f06b7ca39725
Jack Boucher <jboucher56@gmail.com>
2022-06-15 10:53:14 -0500

    Game over and score feature added when you hit the bottom of the block

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
 var jumping = 0;
+var counter = 0;
 
 hole.addEventListener('animationiteration', () => {
     var random = Math.random()*3;
@@ -13,6 +14,11 @@ setInterval(function(){
     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping==0) {
     character.style.top = (characterTop+3) + "px";
+    }
+    if(characterTop>480){
+        alert("Game Over. Score: "+counter);
+        character.style.top = 100 + "px";
+        counter = 0;
     }
 },10);
 
