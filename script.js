762c0ce2d35631048475b28f06d341779f3e4d69
Jack Boucher <jboucher56@gmail.com>
2022-06-15 10:48:23 -0500

    Jump function working

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var block = document.getElementById("block");
 var hole = document.getElementById("hole");
 var character = document.getElementById("character");
+var jumping = 0;
 
 hole.addEventListener('animationiteration', () => {
     var random = Math.random()*3;
@@ -10,6 +11,25 @@ hole.addEventListener('animationiteration', () => {
 setInterval(function(){
     var characterTop = 
     parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    if(jumping==0) {
     character.style.top = (characterTop+3) + "px";
+    }
 },10);
 
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var characterTop =
+        parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&&(jumpCount<15)) {
+        character.style.top = (characterTop-5) + "px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
+}
